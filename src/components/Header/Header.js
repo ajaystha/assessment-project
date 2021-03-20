41f56860b7ac31933242283175556ab1804da4d4
Ajay Shrestha <ajay.shrestha@corelab.it>
2021-03-21 00:41:58 +0100

    move logo component to icons folder and update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,14 @@
-import Logo from '../Logo';
+import Logo from '../Icons/Logo';
 
 import s from './Header.module.css';
 
 export default function Header() {
   return (
     <header className={s.Header}>
-      <div className={s.AppLogo}>
-        <Logo />
+      <div className={s.HeaderWrapper}>
+        <div className={s.AppLogo}>
+          <Logo />
+        </div>
       </div>
     </header>
   );
